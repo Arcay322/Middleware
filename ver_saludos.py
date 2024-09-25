f459f2d6d7a28a6941000c1e8121d8bd49842cd1
Arnie <u22203827@utp.edu.pe>
2024-09-25 10:44:41 -0500

    saludos

--- ver_saludos.py
+++ ver_saludos.py
@@ -4,15 +4,15 @@ import requests
 def ver_saludos():
     response = requests.get("https://middleware-vl7h.onrender.com/saludos/")
 
+    # Verifica el estado de la respuesta
     if response.status_code == 200:
-        saludos = response.json().get("saludos", [])
-        print("Estructura de saludos:", saludos)  # Imprimir la estructura para depuración
-        if saludos:
-            print("Saludos almacenados:")
+        try:
+            saludos = response.json().get("saludos", [])
             for saludo in saludos:
                 print(
                     f"ID: {saludo[0]}, Nombre: {saludo[1]}, Apellido: {saludo[2]}, Edad: {saludo[3]}, Saludo: {saludo[4]}")
-        else:
-            print("No hay saludos almacenados.")
+        except ValueError:
+            print("Error: La respuesta no es un JSON válido.")
+            print("Respuesta del servidor:", response.text)
     else:
-        print(f"Ocurrió un error: {response.status_code} - {response.text}")
+        print(f"Error en la solicitud: {response.status_code} - {response.text}")
